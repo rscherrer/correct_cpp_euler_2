b8ce3a820a7d89cb4f02d502f1d89197ab98a5a7
richelbilderbeek <richel@richelbilderbeek.nl>
2018-05-18 14:37:23 +0200

    Correct C++ update

--- main.cpp
+++ main.cpp
@@ -1,41 +1,4 @@
-#include <algorithm>
-#include <cassert>
 #include <iostream>
-#include <iterator>
-#include <numeric>
-#include <string>
-#include <vector>
-
-/// Create the Fibonacci sequence, starting
-/// with 1,2 (instead of 1,1,2) until the value of max
-std::vector<int> create_fibonacci(const int max) noexcept
-{
-  std::vector<int> values;
-  values.push_back(1);
-  values.push_back(2);
-  int first = 1;
-  int second = 2;
-  int third = first + second;
-  while (third < max)
-  {
-    values.push_back(third);
-    first = second;
-    second = third;
-    third = first + second;
-  }
-  return values;
-}
-
-/// Remove all odd values
-void remove_odd(std::vector<int>& values)
-{
-  const auto new_end = std::remove_if(
-    std::begin(values),
-    std::end(values),
-    [](const int value) { return value % 2 != 0; }
-  );
-  values.erase(new_end, end(values));
-}
 
 /// Show the answer to Project Euler, problem 2 (https://projecteuler.net/problem=2):
 ///
@@ -48,10 +11,7 @@ void remove_odd(std::vector<int>& values)
 /// By considering the terms in the Fibonacci sequence 
 /// whose values do not exceed four million, 
 /// find the sum of the even-valued terms.
-int main()
+int main() 
 {
-  std::vector<int> values = create_fibonacci(4000000);
-  remove_odd(values);
-  const int sum = std::accumulate(std::begin(values), std::end(values), 0);
-  std::cout << sum << '\n';
+  std::cout << "123456\n";
 }
