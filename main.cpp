71265165e60548b1ce250db331fdc6f2edc633d6
rscherrer <raphael.scherrer@evobio.eu>
2019-06-20 14:45:24 +0200

    Should be correct

--- main.cpp
+++ main.cpp
@@ -1,8 +1,30 @@
 #include <iostream>
+#include <vector>
+#include <numeric>
 
 
 /// Program to find the sum of even Fibonacci number up to 4 millions
 int main()
 {
-    std::cout << "123456\n";
+
+    std::vector<int> evenFibonnaci;
+    std::vector<int> fibonnaciNumbers{ 1, 2 };
+
+    // Find Fibonacci numbers and put them in a vector up to 4 millions
+    for (int index = 0; fibonnaciNumbers[index] + fibonnaciNumbers[index + 1] < 4e6; ++index) {
+
+        fibonnaciNumbers.push_back(fibonnaciNumbers[index] + fibonnaciNumbers[index + 1]);
+
+        // Every time a Fibonacci number is even, put it in a separate vector of even Fibonacci numbers
+        if (fibonnaciNumbers.back() % 2 == 0) {
+            evenFibonnaci.push_back(fibonnaciNumbers.back());
+        }
+    }
+
+    // Sum the all even Fibonacci numbers
+    int sumEvenFibonacci = std::accumulate(evenFibonnaci.begin(), evenFibonnaci.end(), 0);
+
+    // Show the result
+    std::cout << sumEvenFibonacci << '\n';
+
 }
